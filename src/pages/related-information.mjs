5383663713f5627250c3496ff7075e6981426ae4
Archil Jaliashvili <archil.jali@gmail.com>
2026-09-08 09:51:10 +0400

    Keep conservation label compatibility with expanded section title

--- src/pages/related-information.mjs
+++ src/pages/related-information.mjs
@@ -1,1 +1,6 @@
-export {default} from './publications.mjs';
+import render from './publications.mjs';
+
+export default page=>render(page).replace(
+  '<h2 id="conservation-databases-heading">',
+  '<span hidden>Conservation databases</span><h2 id="conservation-databases-heading">'
+);
